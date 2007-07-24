c637452935d579daf9a98d6f06b08e26a37640ec
fletcher.e <fletcher.e@cd1a72c9-3235-0410-8280-c7c606c8af80>
2007-07-24 05:56:51 +0000

    manager class with base functions

--- trunk/Calendar/src/manager.py
+++ trunk/Calendar/src/manager.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python
 class manager:
     "Class that manages the calendar"
+    listHead = 'null'
+    def addEntry(date,title):
+        pass   
+    def editEntry(entry, title, location, time, duration):
+        pass
+    def removeEntry(entry):
+        pass
+    def sort(date):
+        pass
+    def complete(entry):
+        pass
+    def exportEntry(entry):
+        pass
