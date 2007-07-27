d8b2daa0623a75ec2defee91b4879ee4941e81ac
fletcher.e <fletcher.e@cd1a72c9-3235-0410-8280-c7c606c8af80>
2007-07-27 22:44:20 +0000

    Added some exceptions

--- Calendar/src/entry.py
+++ Calendar/src/entry.py
@@ -28,3 +28,8 @@ class event(entry):
     def __init__(self, inputTime):
         self.time = inputTime
 
+class entryException(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value) 
--- Calendar/src/manager.py
+++ Calendar/src/manager.py
@@ -1,35 +1,45 @@
 #!/usr/bin/python
 
-
 class manager:
     "Class that manages the calendar"
     listHead = 'null'
     def __init__(self):
         print 'new manager class instantiated'
-
     def addEntry(self, date, title):
+        """adds the entry to the daylist"""
         newEntry = entry(title)
         newEntry.date = date
-        print 'entry added'
-        print newEntry.title
-        self.listHead = [newEntry]
-
+        if newEntry.title == '':
+            #throw error
+            try:
+                raise entryException()
+            except entryException, e:
+                print 'Nonexistant entry found :', e.value 
+        else:
+            print 'entry added'
+            print newEntry.title
+            self.listHead = [newEntry]
     def addEntry(entry):
         print 'entry added'
-
     def editEntry(entry, title, location, time, duration):
-        print 'entry changed'
-
+        """edits specified entry in the list"""
+        pEntry = entry(title)
+        if pEntry == '':
+            #throw error
+            try:
+                raise entryException()
+            except entryException, e:
+                print 'Nonexistant entry found :', e.value 
+        else:
+            #edit entry
+            print 'entry changed'
+            pass
     def removeEntry(entry):
         print 'entry deleted'
-
     def sort(date):
         print 'list re-sorted'
-
     def complete(entry):
         print 'task completed'
-
     def exportEntry(entry):
         print 'entry saved'
-
     print 'done'
