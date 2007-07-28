a20950ad60d4cd56aeab1ada0c06555dba37225a
nathanbrit <nathanbrit@cd1a72c9-3235-0410-8280-c7c606c8af80>
2007-07-28 23:51:41 +0000

    Finished the addEntry method

--- Calendar/src/manager.py
+++ Calendar/src/manager.py
@@ -1,26 +1,45 @@
 #!/usr/bin/python
+from datetime import date
+
 
 class manager:
-    "Class that manages the calendar"
-    listHead = []
+    """Class that manages the calendar"""
+    #dateList essentially is the calendar
+    #takes on the form of:
+    #[[date(2007, 7, 20), [1, 2, 3]], [date(2007, 7, 21), [1, 2, 3, 4]], [date(2007, 7, 22), [1, 2]]]
+    dateList = []
     def __init__(self):
         print 'new manager class instantiated'
-    def addEntry(self, date, title):
+
+    def addEntry(self, title, date):                     #Adds a new entry to a Calendar list
         """adds the entry to the daylist"""
-        newEntry = entry(title)
-        newEntry.date = date
+        newEntry = entry(title, date)
         if newEntry.title == '':
             #throw error
             try:
                 raise entryException()
             except entryException, e:
-                print 'Nonexistant entry found :', e.value 
+                print 'Nonexistant entry found :', e.value
         else:
-            print 'entry added'
-            print newEntry.title
-            self.listHead = [newEntry]
-    def addEntry(self, newEntry):
-        print newEntry.title
+            if len(self.dateList) == 0:                         #If there is nothing in the calendar yet
+                    subList = [newEntry.date, [newEntry]]           #Add the first date and it's first entry
+                    self.dateList.append(subList)
+            else:
+                for i in range(len(self.dateList)):                      #i indexes each date
+                    #print self.dateList[i][0], "<-->", newEntry.date
+                    if self.dateList[i][0] == newEntry.date:                 #If the new entry is of an indexed date
+                        self.dateList[i][1].append(newEntry)                     #Add the entry to the end of the list for that day
+                        break
+                    elif self.dateList[i][0] > newEntry.date:                #If the new entry comes before an indexed date
+                        subList = [newEntry.date, [newEntry]]               #Insert the new day and it's 1st entry
+                        self.dateList.insert(i, subList)
+                        break
+                    elif i == (len(self.dateList)-1):                        #If the new entry comes after all indexed dates
+                        subList = [newEntry.date, [newEntry]]               #Add the new day and it's 1st entry to the end
+                        self.dateList.append(subList)
+                        break
+            print 'entry', newEntry.title, 'added'
+
     def editEntry(entry, title, location, time, duration):
         """edits specified entry in the list"""
         pEntry = entry(title)
@@ -29,17 +48,21 @@ class manager:
             try:
                 raise entryException()
             except entryException, e:
-                print 'Nonexistant entry found :', e.value 
+                print 'Nonexistant entry found :', e.value
         else:
             #edit entry
             print 'entry changed'
             pass
+
     def removeEntry(entry):
         print 'entry deleted'
+
     def sort(date):
         print 'list re-sorted'
+
     def complete(entry):
         print 'task completed'
+
     def exportEntry(entry):
         print 'entry saved'
-    print 'done'
+
